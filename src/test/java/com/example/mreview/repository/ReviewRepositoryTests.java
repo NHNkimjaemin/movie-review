ca7114af2231e9b92b129e95fa5b66d05841b8e5
NHNkimjaemin <jaemin.kim@nhn.com>
2021-01-20 22:50:01 +0900

    test를 통해 dummy data 만들기

---
+++ src/test/java/com/example/mreview/repository/ReviewRepositoryTests.java
@@ -0,0 +1,51 @@
+package com.example.mreview.repository;
+
+
+import com.example.mreview.entity.Member;
+import com.example.mreview.entity.Movie;
+import com.example.mreview.entity.Review;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class ReviewRepositoryTests {
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Test
+    public void insertMovieReviews() {
+
+        IntStream.rangeClosed(1, 200).forEach(i -> {
+            Long mno = (long)(Math.random() * 100) + 1;
+
+            Long mid = ((long)(Math.random()*100) + 1);
+
+            Member member = Member.builder()
+                    .mid(mid)
+                    .build();
+
+            Movie movie = Movie.builder()
+                    .mno(mno)
+                    .build();
+
+            Review movieReview = Review.builder()
+                    .member(member)
+                    .movie(movie)
+                    .grade((int)(Math.random() * 5) + 1)
+                    .text("재미나요" + i)
+                    .build();
+
+
+            reviewRepository.save(movieReview);
+
+
+        });
+    }
+
+
+
+
+}
