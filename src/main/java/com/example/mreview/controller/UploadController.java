ded263dc85cbd3a2b4fa9bde7761952636008d02
NHNkimjaemin <jaemin.kim@nhn.com>
2021-01-21 14:39:47 +0900

    업로드 후 디스플레이 띄우는 기능 까지 완asaㅁㄴㅇ

--- src/main/java/com/example/mreview/controller/UploadController.java
+++ src/main/java/com/example/mreview/controller/UploadController.java
@@ -1,17 +1,29 @@
 package com.example.mreview.controller;
 
+import com.example.mreview.dto.UploadResultDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @RestController
@@ -23,15 +35,16 @@ public class UploadController {
 
 
     @PostMapping("/uploadAjax")
-    public void uploadFile(MultipartFile[] uploadFiles){
+    public ResponseEntity<List<UploadResultDTO>> uploadFile(MultipartFile[] uploadFiles){
+        List<UploadResultDTO> resultDTOList = new ArrayList<>();
 
         for (MultipartFile uploadFile: uploadFiles){
 
 
             // image check
-            if(uploadFile.getContentType().startsWith("image") == false) {
+            if(!Objects.requireNonNull(uploadFile.getContentType()).startsWith("image")) {
                 log.warn("this file is not image type");
-                return;
+                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
             }
 
             String originalName = uploadFile.getOriginalFilename();
@@ -54,12 +67,41 @@ public class UploadController {
 
             try {
                 uploadFile.transferTo(savePath);
+                resultDTOList.add(new UploadResultDTO(fileName, uuid, folderPath));
 
             }catch (IOException e) {
                 e.printStackTrace();
             }
-
         }
+
+        return new ResponseEntity<>(resultDTOList, HttpStatus.OK);
+    }
+
+    @GetMapping("/display")
+    public ResponseEntity<byte[]> getFile(String fileName) {
+
+        ResponseEntity<byte[]> result = null;
+
+        try {
+             String srcFileName = URLDecoder.decode(fileName, "UTF-8");
+
+             log.info("fileName: "+ srcFileName);
+
+             File file = new File(uploadPath + File.separator+ srcFileName);
+
+             log.info("file: ", file);
+
+            HttpHeaders header = new HttpHeaders();
+
+            header.add("Content-type", Files.probeContentType(file.toPath()));
+
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+
+        } catch (Exception e){
+            log.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return result;
     }
 
     private String makeFolder() {
