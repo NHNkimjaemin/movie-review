bf238afd1cc1285895c06ee7920d8491b7693eb4
NHNkimjaemin <jaemin.kim@nhn.com>
2021-01-21 21:52:25 +0900

    chapter9 완료

--- src/main/java/com/example/mreview/controller/UploadController.java
+++ src/main/java/com/example/mreview/controller/UploadController.java
@@ -87,18 +87,23 @@ public class UploadController {
     }
 
     @GetMapping("/display")
-    public ResponseEntity<byte[]> getFile(String fileName) {
+    public ResponseEntity<byte[]> getFile(String fileName, String size) {
 
         ResponseEntity<byte[]> result = null;
 
         try {
-             String srcFileName = URLDecoder.decode(fileName, "UTF-8");
+            String srcFileName = URLDecoder.decode(fileName, "UTF-8");
 
-             log.info("fileName: "+ srcFileName);
+            log.info("fileName: "+ srcFileName);
 
-             File file = new File(uploadPath + File.separator+ srcFileName);
+            File file = new File(uploadPath + File.separator+ srcFileName);
 
-             log.info("file: ", file);
+
+            if(size != null && size.equals("1")) {
+                file = new File(file.getParent(), file.getName().substring(2));
+            }
+
+            log.info("file: ", file);
 
             HttpHeaders header = new HttpHeaders();
 
