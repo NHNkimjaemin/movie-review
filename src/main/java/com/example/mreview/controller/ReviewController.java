e84b2828fbc2dc045016faefbe72be35698f8b69
NHNkimjaemin <jaemin.kim@nhn.com>
2021-01-21 21:46:05 +0900

    리뷰 모달ㄱ 관련 버튼 작업

---
+++ src/main/java/com/example/mreview/controller/ReviewController.java
@@ -0,0 +1,50 @@
+package com.example.mreview.controller;
+
+import com.example.mreview.dto.ReviewDTO;
+import com.example.mreview.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/reviews")
+@Log4j2
+@RequiredArgsConstructor
+public class ReviewController {
+    private final ReviewService reviewService;
+
+    @GetMapping("/{mno}/all")
+    public ResponseEntity<List<ReviewDTO>> getList(@PathVariable("mno") Long mno) {
+        List<ReviewDTO> reviewDTOList = reviewService.getListOfMovie(mno);
+
+        return new ResponseEntity<>(reviewDTOList, HttpStatus.OK);
+    }
+
+    @PostMapping("/{mno}")
+    public ResponseEntity<Long> addReview(@RequestBody ReviewDTO movieReviewDTO) {
+        Long reviewnum = reviewService.register(movieReviewDTO);
+
+        return new ResponseEntity<>(reviewnum, HttpStatus.OK);
+    }
+
+    @PutMapping("/{mno}/{reviewnum}")
+    public ResponseEntity<Long> modifyReview(@PathVariable Long reviewnum, @RequestBody ReviewDTO movieReviewDTO){
+        reviewService.modify(movieReviewDTO);
+
+        return new ResponseEntity<>(reviewnum, HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/{mno}/{reviewnum}")
+    public ResponseEntity<Long> removeReview(@PathVariable Long reviewnum) {
+        reviewService.remove(reviewnum);
+        return new ResponseEntity<>(reviewnum, HttpStatus.OK);
+    }
+
+
+}
